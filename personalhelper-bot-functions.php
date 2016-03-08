abcee0c9303c0c364d5891d1f64f596e308f7a42
lesharodin <lesharodin@gmail.com>
2016-03-08 15:27:53 +0300

    Update personalhelper-bot-functions.php

--- personalhelper-bot-functions.php
+++ personalhelper-bot-functions.php
@@ -16,32 +16,12 @@ $message = $output['message']['text'];
  * Set response.
  */
 switch($message) {
-  // Weather API by OpenWeatherMap.
-  // @see http://api.openweathermap.org
-  case '/pogoda':
-    // App ID.
-    $appid = '<APP_ID_HERE>';
-    // Place (city) ID.
-    // @example St.Petersburg, Russia.
-    $id = '500776';
-    // Weather array.
-    $pogoda = json_decode(file_get_contents('http://api.openweathermap.org/data/2.5/weather?appid=' . $appid . '&id=' . $id . '&units=metric&lang=ru'), TRUE);
-    // Temperature
-    if ($pogoda['main']['temp'] > 0) $temperature = '+' . sprintf("%u", $pogoda['main']['temp']);
-    else $temperature = sprintf("%u", $pogoda['main']['temp']);
-    // Wind direcrion.
-    if ($pogoda['wind']['deg'] >= 0 && $pogoda['wind']['deg'] <= 11.25) $wind_direction = 'северный';
-    elseif ($pogoda['wind']['deg'] > 11.25 && $pogoda['wind']['deg'] <= 78.75) $wind_direction = 'северо-восточный, ';
-    elseif ($pogoda['wind']['deg'] > 78.75 && $pogoda['wind']['deg'] <= 101.25) $wind_direction = 'восточный, ';
-    elseif ($pogoda['wind']['deg'] > 101.25 && $pogoda['wind']['deg'] <= 168.75) $wind_direction = 'юго-восточный, ';
-    elseif ($pogoda['wind']['deg'] > 168.75 && $pogoda['wind']['deg'] <= 191.25) $wind_direction = 'южный, ';
-    elseif ($pogoda['wind']['deg'] > 191.25 && $pogoda['wind']['deg'] <= 258.75) $wind_direction = 'юго-западный, ';
-    elseif ($pogoda['wind']['deg'] > 258.75 && $pogoda['wind']['deg'] <= 281.25) $wind_direction = 'западный, ';
-    elseif ($pogoda['wind']['deg'] > 281.25 && $pogoda['wind']['deg'] <= 348.75) $wind_direction = 'северо-западный, ';
-    else $wind_direction = '';
+  
+  case '/test':
+    
     // Set text.
-    $text = 'Сейчас ' . $pogoda['weather'][0]['description'] . '. Температура воздуха: ' . $temperature . '°C. Ветер ' . $wind_direction . sprintf("%u", $pogoda['wind']['speed']) . ' м/сек.';
-    // Send weather to Telegram user.
+    $text = 'hello world';
+    
     sendMessage($chat_id, $text);
     break;
   default:
