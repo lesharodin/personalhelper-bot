3b09ddfa3f44944fd5c88fabca4c798550632065
lesharodin <lesharodin@gmail.com>
2016-03-08 15:41:58 +0300

    Update personalhelper-bot-functions.php

--- personalhelper-bot-functions.php
+++ personalhelper-bot-functions.php
@@ -1,25 +1,17 @@
 <?php
-/**
- * Telegram Bot access token and API url.
- */
+
 $access_token = '<152179862:AAH5HvR_hymAZElzLJ8ZNNdRqrlMBoSReMo>';
 $api = 'https://api.telegram.org/bot' . $access_token;
 
-/**
- * Listen commands, set chat ID and message.
- */
 $output = json_decode(file_get_contents('php://input'), TRUE);
 $chat_id = $output['message']['chat']['id'];
 $message = $output['message']['text'];
 
-/**
- * Set response.
- */
 switch($message) {
   
   case '/test':
     
-    // Set text.
+    
     $text = 'hello world';
     
     sendMessage($chat_id, $text);
@@ -28,9 +20,6 @@ switch($message) {
     break;
 }
 
-/**
- * Function sendMessage().
- */
 function sendMessage($chat_id, $message) {
   file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
 }
